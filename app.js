03bef2ad1632e7e80a9298c2a90112784ba8a83a
Nehil Danis <nehild@outlook.com>
2024-11-27 22:10:20 +0100

    FINISHED: core functionality

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function calculateAttackForce(min, max) {
+function getRandomValue(min, max) {
     if (max <= min) {
         return 0;
     }
@@ -10,36 +10,82 @@ const app = Vue.createApp({
         return {
             playerHealth: 100,
             monsterHealth: 100,
-            currentRound: 0
+            currentRound: 0,
+            winner: null
         };
     },
     computed: {
         monsterBarStyles() {
+            if (this.monsterHealth < 0) {
+                return { width: '0%' };
+            }
             return { width: this.monsterHealth + '%' }
         },
         playerBarStyles() {
+            if (this.playerHealth < 0) {
+                return { width: '0%' };
+            }
             return { width: this.playerHealth + '%' }
         },
         mayUseSpecialAttack() {
             return this.currentRound % 3 !== 0;
         }
     },
+    watch: {
+        playerHealth(value) {
+            if (value <= 0 && this.monsterHealth <= 0) {
+                this.winner = 'draw';
+            }
+            else if (value <= 0) {
+                this.winner = 'monster';
+            }
+        },
+        monsterHealth(value) {
+            if (value <= 0 && this.playerHealth <= 0) {
+                this.winner = 'draw';
+            }
+            else if (value <= 0) {
+                this.winner = 'player';
+            }
+        }
+
+    },
     methods: {
         attackMonster() {
-            this.currentRound += 1;
-            const attachValue = calculateAttackForce(5, 12);
+            this.currentRound++;
+            const attachValue = getRandomValue(5, 12);
             this.monsterHealth -= attachValue;
             this.attackPlayer();
         },
         attackPlayer() {
-            const attachValue = calculateAttackForce(8, 15);
+            const attachValue = getRandomValue(8, 15);
             this.playerHealth -= attachValue;
         },
         specialAttackMonster() {
             this.currentRound++;
-            const attachValue = calculateAttackForce(10, 25);
+            const attachValue = getRandomValue(10, 25);
             this.monsterHealth -= attachValue;
             this.attackPlayer();
+        },
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRandomValue(8, 20);
+            if (this.playerHealth + healValue > 100) {
+                this.playerHealth = 100
+            }
+            else {
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();
+        },
+        startNewGame() {
+            this.winner = null;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.currentRound = 0;
+        },
+        surrender() {
+            this.winner = 'monster';
         }
     }
 
